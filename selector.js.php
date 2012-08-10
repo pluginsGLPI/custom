7e6220a4ec657f2202afa8b7e9e04f7b13374232
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2012-08-10 09:57:55 +0200

    add debug toggle feature

--- selector.js.php
+++ selector.js.php
@@ -56,18 +56,45 @@ if ($plugin->isInstalled("custom") && $plugin->isActivated("custom")) {
 
       echo "}";
 
+      $JS = <<<JAVASCRIPT
       //add a star to user group vip
-      echo "
       Ext.select('span.x-hidden').each(function(el){
          if (el.dom.innerHTML.indexOf('VIP') > 0) {
             el.insertHtml(
                'beforeBegin',
-               '<img src=\"../plugins/custom/pics/vip.png\" alt=\"VIP\" title=\"VIP\" />'
+               "<img src='../plugins/custom/pics/vip.png' alt='VIP' title='VIP' />"
             );
          }
       });
-      ";
-   echo "});";
+
+
+      //add a toggle button on debug h2 elements
+      Ext.select('#debug h2').each(function(el){
+         el.insertHtml(
+            'afterBegin',
+            "<a href='#' class='toggle_debug'><img alt='' class='toggle_debug_img' src='/glpi/0.84-bugfixes/pics/deplier_down.png'></a>&nbsp;"
+         );
+      }).on('click', function() {
+         //toggle element under h2 clicked
+         Ext.select('#'+this.id+' + table').toggle();
+         Ext.select('#'+this.id+' + p').toggle();
+
+         //toggle img
+         var sel_img = Ext.select('#'+this.id+' .toggle_debug_img');
+         var src_img = sel_img.elements[0].src;
+         if (src_img.indexOf('down') > 0) {
+            sel_img.elements[0].src = '/glpi/0.84-bugfixes/pics/deplier_up.png';
+         } else {
+            sel_img.elements[0].src = '/glpi/0.84-bugfixes/pics/deplier_down.png';
+         }
+      });
+
+      //set toggle function to add "display:none" instead "visibility:hidden"
+      Ext.select('#debug>table, #debug>p').setVisibilityMode(Ext.Element.DISPLAY);
+      Ext.select('#debug>table, #debug>p').toggle();
+   });
+JAVASCRIPT;
+echo $JS;
 }
 
 ?>
