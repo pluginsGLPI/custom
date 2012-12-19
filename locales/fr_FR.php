e1c1e1cd1cbab2b74f173bcbe1172f59abe12d0e
Alexandre <adelaunay@teclib.com>
2012-12-19 10:19:11 +0100

    fix locales files (missing en_GB entries)

--- locales/fr_FR.php
+++ locales/fr_FR.php
@@ -1,24 +1,24 @@
 <?php
-$LANG['plugin_custom']['name']="Custom";
+$LANG['plugin_custom']['name']             = "Custom";
 
-$LANG['plugin_custom']['title'][0] = "Onglets colorés";
-$LANG['plugin_custom']['title'][1] = "Onglets par défaut";
-$LANG['plugin_custom']['title'][2] = "Style de GLPI";
+$LANG['plugin_custom']['title'][0]         = "Onglets colorés";
+$LANG['plugin_custom']['title'][1]         = "Onglets par défaut";
+$LANG['plugin_custom']['title'][2]         = "Style de GLPI";
 
-$LANG['plugin_custom']['type'][0] = "onglet coloré";
-$LANG['plugin_custom']['type'][1] = "onglet par défaut";
-$LANG['plugin_custom']['type'][2] = "style";
+$LANG['plugin_custom']['type'][0]          = "onglet coloré";
+$LANG['plugin_custom']['type'][1]          = "onglet par défaut";
+$LANG['plugin_custom']['type'][2]          = "style";
 
-$LANG['plugin_custom']['form'][0] = "Onglet";
-$LANG['plugin_custom']['form'][1] = "Couleur";
+$LANG['plugin_custom']['form'][0]          = "Onglet";
+$LANG['plugin_custom']['form'][1]          = "Couleur";
 
-$LANG['plugin_custom']['config'][0] = "Colorer/supprimer des onglets";
-$LANG['plugin_custom']['config'][1] = "Onglets par défaut";
-$LANG['plugin_custom']['config'][2] = "Modifier le style de GLPI";
+$LANG['plugin_custom']['config'][0]        = "Colorer/supprimer des onglets";
+$LANG['plugin_custom']['config'][1]        = "Onglets par défaut";
+$LANG['plugin_custom']['config'][2]        = "Modifier le style de GLPI";
 
-$LANG['plugin_custom']['profile'][1] = "Editer les couleurs aux onglets";
-$LANG['plugin_custom']['profile'][2] = "Définir les onglets par défaut";
-$LANG['plugin_custom']['profile'][3] = "Modifier le style de GLPI";
+$LANG['plugin_custom']['profile'][1]       = "Editer les couleurs aux onglets";
+$LANG['plugin_custom']['profile'][2]       = "Définir les onglets par défaut";
+$LANG['plugin_custom']['profile'][3]       = "Modifier le style de GLPI";
 
 $LANG['plugin_custom']['color']['deleted'] = "supprimé";
 $LANG['plugin_custom']['color']['red']     = "rouge";
